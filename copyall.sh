4b3002a841d5b49a75c8c95943ec00a1a4728af3
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-02-05 22:03:25 +0100

    Including the buildscript

---
+++ copyall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export GOOS=linux
+export CGO_ENABLED=0
+
+cd accountservice;go get;go build -o accountservice-linux-amd64;echo built `pwd`;cd ..
+
+export GOOS=darwin
+
+docker build -t someprefix/accountservice accountservice/
