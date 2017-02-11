1b31deeaf721f4b37ce74e1203ea360b79d18765
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-02-11 17:50:47 +0100

    Reverted back to P2 state

--- accountservice/main.go
+++ accountservice/main.go
@@ -3,19 +3,12 @@ package main
 import (
         "fmt"
         "github.com/callistaenterprise/goblog/accountservice/service"
-        "github.com/callistaenterprise/goblog/accountservice/dbclient"
 )
 
 var appName = "accountservice"
 
 func main() {
         fmt.Printf("Starting %v\n", appName)
-        initializeBoltClient()
         service.StartWebServer("6767")
 }
 
-func initializeBoltClient() {
-        service.DBClient = &dbclient.BoltClient{}
-        service.DBClient.OpenBoltDb()
-        service.DBClient.Seed()
-}
--- accountservice/service/routes.go
+++ accountservice/service/routes.go
@@ -20,6 +20,9 @@ var routes = Routes{
                 "GetAccount",             // Name
                 "GET",                    // HTTP method
                 "/accounts/{accountId}",  // Route pattern
-                GetAccount,
+                func(w http.ResponseWriter, r *http.Request) {
+                        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+                        w.Write([]byte("{\"result\":\"OK\"}"))
+                },
         },
 }
