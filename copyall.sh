6f9c6d44209c1acfd0e8187947a632c594457558
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-02-12 21:52:39 +0100

    Updated the copyall script

--- copyall.sh
+++ copyall.sh
@@ -7,3 +7,6 @@ cd accountservice;go get;go build -o accountservice-linux-amd64;echo built `pwd`
 export GOOS=darwin
 
 docker build -t someprefix/accountservice accountservice/
+
+docker service rm accountservice
+docker service create --name=accountservice --replicas=1 --network=my_network -p=6767:6767 someprefix/accountservice
