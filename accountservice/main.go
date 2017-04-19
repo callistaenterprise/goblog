d08e72be570364c4bde7acd01bccfeb265822457
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-04-19 12:55:36 +0200

    Updated part 7 with gock http outbound mocking

--- accountservice/main.go
+++ accountservice/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-        "fmt"
-        "github.com/callistaenterprise/goblog/accountservice/service"
-        "github.com/callistaenterprise/goblog/accountservice/dbclient"
+	"fmt"
+	"github.com/callistaenterprise/goblog/accountservice/dbclient"
+	"github.com/callistaenterprise/goblog/accountservice/service"
 )
 
 var appName = "accountservice"
 
 func main() {
-        fmt.Printf("Starting %v\n", appName)
-        initializeBoltClient()
-        service.StartWebServer("6767")
+	fmt.Printf("Starting %v\n", appName)
+	initializeBoltClient()
+	service.StartWebServer("6767")
 }
 
 func initializeBoltClient() {
-        service.DBClient = &dbclient.BoltClient{}
-        service.DBClient.OpenBoltDb()
-        service.DBClient.Seed()
+	service.DBClient = &dbclient.BoltClient{}
+	service.DBClient.OpenBoltDb()
+	service.DBClient.Seed()
 }
--- accountservice/model/account.go
+++ accountservice/model/account.go
@@ -4,6 +4,13 @@ type Account struct {
         Id string `json:"id"`
         Name string  `json:"name"`
         ServedBy string `json:"servedBy"`
+        Quote Quote `json:"quote"`
+}
+
+type Quote struct {
+        Text string `json:"quote"`
+        ServedBy string `json:"ipAddress"`
+        Language string `json:"language"`
 }
 
 func (a *Account) ToString() string {
