5d77ba57416d245c08d33819d96817d599d22c69
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-04-24 08:20:01 +0200

    Formatting

--- accountservice/main.go
+++ accountservice/main.go
@@ -2,31 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/callistaenterprise/goblog/accountservice/config"
 	"github.com/callistaenterprise/goblog/accountservice/dbclient"
 	"github.com/callistaenterprise/goblog/accountservice/service"
-        "github.com/spf13/viper"
-        "github.com/callistaenterprise/goblog/accountservice/config"
+	"github.com/spf13/viper"
+	"flag"
 )
 
 var appName = "accountservice"
 
 func main() {
 	fmt.Printf("Starting %v\n", appName)
-        parseFlags()
-        config.LoadConfiguration(viper.GetString("configServerUrl"), appName, viper.GetString("profile"))
+	parseFlags()
+	config.LoadConfiguration(viper.GetString("configServerUrl"), appName, viper.GetString("profile"))
 	initializeBoltClient()
 	service.StartWebServer("6767")
 }
 
 func parseFlags() {
-        profile := *flag.String("profile", "test", "Environment profile, something similar to spring profiles")
-        configServerUrl := *flag.String("configServerUrl", "http://configserver:8888", "Address to config server")
-        flag.Parse()
-        viper.Set("profile", profile)
-        viper.Set("configServerUrl", configServerUrl)
+	profile := *flag.String("profile", "test", "Environment profile, something similar to spring profiles")
+	configServerUrl := *flag.String("configServerUrl", "http://configserver:8888", "Address to config server")
+	flag.Parse()
+	viper.Set("profile", profile)
+	viper.Set("configServerUrl", configServerUrl)
 }
-
-
 
 func initializeBoltClient() {
 	service.DBClient = &dbclient.BoltClient{}
