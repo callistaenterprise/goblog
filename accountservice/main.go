cee5f7610db5cc794920c33272027472362c4100
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-05-12 15:06:27 +0200

    Last major update of Part 8

--- accountservice/main.go
+++ accountservice/main.go
@@ -1,30 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/callistaenterprise/goblog/accountservice/config"
 	"github.com/callistaenterprise/goblog/accountservice/dbclient"
 	"github.com/callistaenterprise/goblog/accountservice/service"
 	"github.com/spf13/viper"
-	"flag"
 )
 
 var appName = "accountservice"
 
-func main() {
-	fmt.Printf("Starting %v\n", appName)
-	parseFlags()
-	config.LoadConfiguration(viper.GetString("configServerUrl"), appName, viper.GetString("profile"))
-	initializeBoltClient()
-	service.StartWebServer("6767")
+func init() {
+	profile := flag.String("profile", "test", "Environment profile, something similar to spring profiles")
+	configServerUrl := flag.String("configServerUrl", "http://configserver:8888", "Address to config server")
+	configBranch := flag.String("configBranch", "master", "git branch to fetch configuration from")
+
+	flag.Parse()
+
+	fmt.Println("Specified configBranch is " + *configBranch)
+
+	viper.Set("profile", *profile)
+	viper.Set("configServerUrl", *configServerUrl)
+	viper.Set("configBranch", *configBranch)
 }
 
-func parseFlags() {
-	profile := *flag.String("profile", "test", "Environment profile, something similar to spring profiles")
-	configServerUrl := *flag.String("configServerUrl", "http://configserver:8888", "Address to config server")
-	flag.Parse()
-	viper.Set("profile", profile)
-	viper.Set("configServerUrl", configServerUrl)
+func main() {
+	fmt.Printf("Starting %v\n", appName)
+
+	config.LoadConfigurationFromBranch(
+		viper.GetString("configServerUrl"),
+		appName,
+		viper.GetString("profile"),
+		viper.GetString("configBranch"))
+	initializeBoltClient()
+	go config.StartListener(appName, viper.GetString("amqp_server_url"), viper.GetString("config_event_bus"))
+	service.StartWebServer(viper.GetString("server_port"))
 }
 
 func initializeBoltClient() {
