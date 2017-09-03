6dc5e677353d9a174388203cb275782ebd8a856c
Erik Lupander <erik.lupander@callistaenterprise.se>
2017-09-03 11:21:58 +0200

    Refactoring and test code for the circuit breaker

--- common/circuitbreaker/hystrix.go
+++ common/circuitbreaker/hystrix.go
@@ -1,129 +1,156 @@
 package circuitbreaker
 
 import (
-	"encoding/json"
-	"github.com/afex/hystrix-go/hystrix"
-	"github.com/spf13/viper"
-	"net"
-	"net/http"
-	"time"
-	"github.com/callistaenterprise/goblog/common/messaging"
-	"github.com/Sirupsen/logrus"
-	"github.com/callistaenterprise/goblog/common/util"
-	"github.com/eapache/go-resiliency/retrier"
-	"io/ioutil"
+        "encoding/json"
+        "github.com/afex/hystrix-go/hystrix"
+        "github.com/spf13/viper"
+        "net"
+        "net/http"
+        "time"
+        "github.com/callistaenterprise/goblog/common/messaging"
+        "github.com/Sirupsen/logrus"
+        "github.com/callistaenterprise/goblog/common/util"
+        "github.com/eapache/go-resiliency/retrier"
+        "io/ioutil"
         "fmt"
+        "log"
 )
 
-var client http.Client
-
-func Call(breakerName string, url string, method string) ([]byte, error) {
-	output := make(chan []byte, 1)
-	errors := hystrix.Go(breakerName, func() error {
-		// talk to other services
-		req, _ := http.NewRequest(method, url, nil)
-
-		r := retrier.New(retrier.ConstantBackoff(3, 100*time.Millisecond), nil)
-		times := 0
-		err := r.Run(func() error {
-                        logrus.Infof("Calling %v\n", times)
-			resp, err := client.Do(req)
-                        logrus.Infof("Returning %v\n", resp.StatusCode)
-			if err == nil && resp.StatusCode < 299 {
-				responseBody, err := ioutil.ReadAll(resp.Body)
-				if err == nil {
-					output <- responseBody
-					return nil
-				}
-                                return err
-			} else if err != nil {
-                                times++
-                                logrus.Errorf("Attempt failed. Retrier: %v", times)
-                        } else {
-                                err = fmt.Errorf("Status was %v", resp.StatusCode)
-                        }
-
-			return err
-		})
-
-		// For hystrix, forward the err from the retrier. It's nil if OK.
-		return err
-	}, func(err error) error {
-		logrus.Errorf("Breaker %v opened, error: %v", breakerName, err.Error())
-		return err
-	})
-
-	select {
-	case out := <-output:
-		return out, nil
-
-	case err := <-errors:
-		return nil, err
-	}
-	return nil, nil
+func init() {
+        log.SetOutput(ioutil.Discard)
 }
 
+var Client http.Client
+
+var RETRIES = 3
+
+func CallUsingCircuitBreaker(breakerName string, url string, method string) ([]byte, error) {
+        output := make(chan []byte, 1)
+        errors := hystrix.Go(breakerName, func() error {
+
+                req, _ := http.NewRequest(method, url, nil)
+                err := callWithRetries(req, output)
+
+                return err     // For hystrix, forward the err from the retrier. It's nil if OK.
+        }, func(err error) error {
+                logrus.Errorf("In fallback function for breaker %v, error: %v", breakerName, err.Error())
+                circuit, _, _ := hystrix.GetCircuit(breakerName)
+                logrus.Errorf("Circuit state is: %v", circuit.IsOpen())
+                return err
+        })
+
+        select {
+        case out := <-output:
+                logrus.Debugf("Call in breaker %v successful", breakerName)
+                return out, nil
+
+        case err := <-errors:
+                return nil, err
+        }
+}
+
+func callWithRetries(req *http.Request, output chan []byte) error {
+
+        r := retrier.New(retrier.ConstantBackoff(RETRIES, 100 * time.Millisecond), nil)
+        attempt := 0
+        err := r.Run(func() error {
+                attempt++
+                resp, err := Client.Do(req)
+                if err == nil && resp.StatusCode < 299 {
+                        responseBody, err := ioutil.ReadAll(resp.Body)
+                        if err == nil {
+                                output <- responseBody
+                                return nil
+                        }
+                        return err
+                } else if err == nil {
+                        err = fmt.Errorf("Status was %v", resp.StatusCode)
+                }
+
+                logrus.Errorf("Retrier failed, attempt %v", attempt)
+
+                return err
+        })
+        return err
+}
 
 func ConfigureHystrix(commands []string, amqpClient messaging.IMessagingClient) {
 
-	for _, command := range commands {
-		hystrix.ConfigureCommand(command, hystrix.CommandConfig{
-			Timeout:                resolveProperty(command, "Timeout"),
-			MaxConcurrentRequests:  resolveProperty(command, "MaxConcurrentRequests"),
-			ErrorPercentThreshold:  resolveProperty(command, "ErrorPercentThreshold"),
-			RequestVolumeThreshold: resolveProperty(command, "RequestVolumeThreshold"),
-			SleepWindow:            resolveProperty(command, "SleepWindow"),
-		})
-		logrus.Printf("Circuit %v settings: %v", command, hystrix.GetCircuitSettings()[command])
-	}
+        for _, command := range commands {
+                hystrix.ConfigureCommand(command, hystrix.CommandConfig{
+                        Timeout:                resolveProperty(command, "Timeout"),
+                        MaxConcurrentRequests:  resolveProperty(command, "MaxConcurrentRequests"),
+                        ErrorPercentThreshold:  resolveProperty(command, "ErrorPercentThreshold"),
+                        RequestVolumeThreshold: resolveProperty(command, "RequestVolumeThreshold"),
+                        SleepWindow:            resolveProperty(command, "SleepWindow"),
+                })
+                logrus.Printf("Circuit %v settings: %v", command, hystrix.GetCircuitSettings()[command])
+        }
 
-	hystrixStreamHandler := hystrix.NewStreamHandler()
-	hystrixStreamHandler.Start()
-	go http.ListenAndServe(net.JoinHostPort("", "8181"), hystrixStreamHandler)
-	logrus.Println("Launched hystrixStreamHandler at 8181")
+        hystrixStreamHandler := hystrix.NewStreamHandler()
+        hystrixStreamHandler.Start()
+        go http.ListenAndServe(net.JoinHostPort("", "8181"), hystrixStreamHandler)
+        logrus.Infoln("Launched hystrixStreamHandler at 8181")
 
-	// Publish presence on RabbitMQ
-	publishDiscoveryToken(amqpClient)
+        // Publish presence on RabbitMQ
+        publishDiscoveryToken(amqpClient)
+}
+
+func Deregister(amqpClient messaging.IMessagingClient) {
+        ip, err := util.ResolveIpFromHostsFile()
+        if err != nil {
+                ip = util.GetIPWithPrefix("10.0.")
+        }
+        token := DiscoveryToken{
+                State:   "DOWN",
+                Address: ip,
+        }
+        bytes, _ := json.Marshal(token)
+        amqpClient.PublishOnQueue(bytes, "discovery")
 }
 
 func publishDiscoveryToken(amqpClient messaging.IMessagingClient) {
-	token := DiscoveryToken{
-		State:   "UP",
-		Address: util.GetIP(),
-	}
-	json, _ := json.Marshal(token)
-	go func() {
-		for {
-			amqpClient.PublishOnQueue(json, "discovery")  //SendMessage(string(json), "application/json", "discovery")
-			time.Sleep(time.Second * 30)
-		}
-	}()
+        ip, err := util.ResolveIpFromHostsFile()
+        if err != nil {
+                ip = util.GetIPWithPrefix("10.0.")
+        }
+        token := DiscoveryToken{
+                State:   "UP",
+                Address: ip,
+        }
+        bytes, _ := json.Marshal(token)
+        go func() {
+                for {
+                        amqpClient.PublishOnQueue(bytes, "discovery")
+                        time.Sleep(time.Second * 30)
+                }
+        }()
 }
 
 func resolveProperty(command string, prop string) int {
-	if viper.IsSet("hystrix.command." + command + "." + prop) {
-		return viper.GetInt("hystrix.command." + command + "." + prop)
-	} else {
-		return getDefaultHystrixConfigPropertyValue(prop)
-	}
+        if viper.IsSet("hystrix.command." + command + "." + prop) {
+                return viper.GetInt("hystrix.command." + command + "." + prop)
+        } else {
+                return getDefaultHystrixConfigPropertyValue(prop)
+        }
 }
 func getDefaultHystrixConfigPropertyValue(prop string) int {
-	switch prop {
-	case "Timeout":
-		return hystrix.DefaultTimeout
-	case "MaxConcurrentRequests":
-		return hystrix.DefaultMaxConcurrent
-	case "RequestVolumeThreshold":
-		return hystrix.DefaultVolumeThreshold
-	case "SleepWindow":
-		return hystrix.DefaultSleepWindow
-	case "ErrorPercentThreshold":
-		return hystrix.DefaultErrorPercentThreshold
-	}
-	panic("Got unknown hystrix property: " + prop + ". Panicing!")
+        switch prop {
+        case "Timeout":
+                return hystrix.DefaultTimeout
+        case "MaxConcurrentRequests":
+                return hystrix.DefaultMaxConcurrent
+        case "RequestVolumeThreshold":
+                return hystrix.DefaultVolumeThreshold
+        case "SleepWindow":
+                return hystrix.DefaultSleepWindow
+        case "ErrorPercentThreshold":
+                return hystrix.DefaultErrorPercentThreshold
+        }
+        panic("Got unknown hystrix property: " + prop + ". Panicing!")
 }
 
 type DiscoveryToken struct {
-	State string `json:"state"`   // UP, RUNNING, DOWN ??
-	Address string `json:"address"`
+        State   string `json:"state"` // UP, RUNNING, DOWN ??
+        Address string `json:"address"`
 }
--- common/circuitbreaker/hystrix_test.go
+++ common/circuitbreaker/hystrix_test.go
@@ -4,24 +4,28 @@ import (
         "gopkg.in/h2non/gock.v1"
         . "github.com/smartystreets/goconvey/convey"
         "testing"
+        "github.com/Sirupsen/logrus"
+        "github.com/afex/hystrix-go/hystrix"
 )
 
-
+func init() {
+        logrus.SetFormatter(&logrus.TextFormatter{
+                FullTimestamp: true,
+                TimestampFormat: "2006-01-02T15:04:05.000",
+        })
+        logrus.SetLevel(logrus.DebugLevel)
+}
 
 func TestCallUsingResilienceAllFails(t *testing.T) {
         defer gock.Off()
 
-        for a := 0 ; a < 4; a++ {
-                gock.New("http://quotes-service:8080").
-                        Get("/api/quote").
-                        MatchParam("strength", "4").
-                        Reply(500)
-        }
+        buildGockMatcherTimes(500, 4)
+        hystrix.Flush()
 
-        Convey("Given a Call request", t, func() {
+        Convey("Given that we've mocked 4 requests to return 500 Server Error", t, func() {
 
-                Convey("When", func() {
-                        bytes, err := Call("TEST", "http://quotes-service:8080/api/quote?strength=4", "GET")
+                Convey("When ", func() {
+                        bytes, err := CallUsingCircuitBreaker("TEST", "http://quotes-service", "GET")
 
                         Convey("Then", func() {
                                 So(err, ShouldNotBeNil)
@@ -32,31 +36,64 @@ func TestCallUsingResilienceAllFails(t *testing.T) {
 }
 
 
-func TestCallUsingResilienceLastSucceedsFails(t *testing.T) {
+func TestCallUsingResilienceLastSucceeds(t *testing.T) {
         defer gock.Off()
-
-        for a := 0 ; a < 3; a++ {
-                gock.New("http://quotes-service:8080").
-                        Get("/api/quote").
-                        MatchParam("strength", "4").
-                        Reply(500)
-        }
-        gock.New("http://quotes-service:8080").
-                Get("/api/quote").
-                MatchParam("strength", "4").
-                Reply(200).
-                BodyString(`{"quote":"May the source be with you. Always.","ipAddress":"10.0.0.5:8080","language":"en"}`)
-
+        RETRIES = 3
+        buildGockMatcherTimes(500, 2)
+        body := []byte("Some response")
+        buildGockMatcherWithBody(200, string(body))
+        hystrix.Flush()
 
         Convey("Given a Call request", t, func() {
 
                 Convey("When", func() {
-                        bytes, err := Call("TEST", "http://quotes-service:8080/api/quote?strength=4", "GET")
+                        bytes, err := CallUsingCircuitBreaker("TEST", "http://quotes-service", "GET")
 
                         Convey("Then", func() {
                                 So(err, ShouldBeNil)
                                 So(bytes, ShouldNotBeNil)
+                                So(string(bytes), ShouldEqual, string(body))
                         })
                 })
         })
+}
+
+func TestCallHystrixOpensAfterThresholdPassed(t *testing.T) {
+        defer gock.Off()
+        for a := 0 ; a < 6; a++ {
+                buildGockMatcher(500)
+        }
+        hystrix.Flush()
+
+        Convey("Given hystrix allows 5 failed requests with no retries", t, func() {
+                RETRIES = 0
+                hystrix.ConfigureCommand("TEST", hystrix.CommandConfig{
+                        RequestVolumeThreshold: 5,
+                })
+                Convey("When 6 failed requests performed", func() {
+                        for a := 0 ; a < 6; a++ {
+                                CallUsingCircuitBreaker("TEST", "http://quotes-service", "GET")
+                        }
+
+                        Convey("Then make sure the circuit has been opened", func() {
+                                cb, _, _ := hystrix.GetCircuit("TEST")
+                                So(cb.IsOpen(), ShouldBeTrue)
+                        })
+                })
+        })
+}
+
+func buildGockMatcherTimes(status int, times int) {
+        for a := 0 ; a < times; a++ {
+                buildGockMatcher(status)
+        }
+}
+
+func buildGockMatcherWithBody(status int, body string) {
+        gock.New("http://quotes-service").
+                Reply(status).BodyString(body)
+}
+
+func buildGockMatcher(status int) {
+       buildGockMatcherWithBody(status, "")
 }
