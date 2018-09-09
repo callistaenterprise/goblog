69aeb98b93d60c7de1ecb9e2ff01e60c68133a2f
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-09-09 16:04:44 +0200

    New bash script for quick rebuild and redeploy of the accountservice

---
+++ as.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+export GOOS=linux
+export CGO_ENABLED=0
+
+cd accountservice;go get;go build -o accountservice-linux-amd64;echo built `pwd`;cd ..
+
+export GOOS=darwin
+
+cp healthchecker/healthchecker-linux-amd64 accountservice/
+
+docker build -t someprefix/accountservice accountservice/
+docker service rm accountservice
+docker service create  --name=accountservice --replicas=1 --network=my_network -p=6767:6767 someprefix/accountservice
